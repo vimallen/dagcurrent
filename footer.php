72ca2fb401f9c505adf5da3233967ba230524201
Dave Allen <davidallen401@btinternet.com>
2019-03-10 20:44:01 +0000

    Page work

--- footer.php
+++ footer.php
@@ -14,19 +14,7 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'daveallengraphics' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'daveallengraphics' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'daveallengraphics' ), 'daveallengraphics', '<a href="http://www.daveallengraphics.com/http://www.daveallengraphics.com/">Dave Allen</a>' );
-				?>
-		</div><!-- .site-info -->
+		
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
